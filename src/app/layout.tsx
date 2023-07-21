659823ed2c2c932dae399f100fcbcb4fae6c4dae
Youka <72034970+yrnmsk@users.noreply.github.com>
2023-07-21 21:14:57 +0800

    fix(layout): `main` width/height

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en'>
       <body className='w-screen h-screen bg-slate-900'>
-        <main className='flex flex-col justify-center items-center'>
+        <main className='w-full h-full flex flex-col justify-center items-center'>
           {children}
         </main>
       </body>
