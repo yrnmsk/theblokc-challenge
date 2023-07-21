91cf04f7e0cfb976203667e68986db6f6585f08f
Youka <72034970+yrnmsk@users.noreply.github.com>
2023-07-21 21:11:03 +0800

    feat(page): add basic button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image";
+
 export default function Page() {
-  return <></>;
+  return (
+    <button className='px-4 py-3 bg-orange-700/90 text-slate-50 border border-slate-500 rounded-lg flex flex-row gap-2 justify-center items-center md:px-6 md:py-4 md:rounded-xl md:text-lg hover:bg-orange-700/70'>
+      <span>Connect with MetaMask</span>
+      <Image className='h-auto' src='/icon-metamask.svg' alt='metamask logo' width={32} height={32} />
+    </button>
+  );
 }
