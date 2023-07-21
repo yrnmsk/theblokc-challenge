db96dd86784a3d520cfd79f158701cecc3476e26
Youka <72034970+yrnmsk@users.noreply.github.com>
2023-07-22 00:36:15 +0800

    feat(page): improve button
    * feat: remove bg-color
    * feat: add gradient colors
    * feat: add transitions
    * feat: improve hover states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Page() {
   return (
-    <button className='px-4 py-3 bg-orange-700/90 text-slate-50 border border-slate-500 rounded-lg flex flex-row gap-2 justify-center items-center md:px-6 md:py-4 md:rounded-xl md:text-lg hover:bg-orange-700/70'>
+    <button className='px-4 py-3 bg-gradient-to-br from-sky-500/75 from-60% to-indigo-500/50 shadow-md shadow-slate-950 transition-all ease-out duration-75 text-slate-50 border border-sky-900 rounded-lg flex flex-row gap-2 justify-center items-center md:px-6 md:py-4 md:rounded-xl md:text-lg hover:from-sky-500/50 hover:from-30% hover:to-indigo-500/30 hover:border-sky-400 hover:shadow-indigo-900 hover:translate-x-1 hover:-translate-y-1'>
       <span>Connect with MetaMask</span>
       <Image className='h-auto' src='/icon-metamask.svg' alt='metamask logo' width={32} height={32} />
     </button>
