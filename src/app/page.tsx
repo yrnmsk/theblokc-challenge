6b05eb79bc77268538b05d12fd6993b177b89af9
Youka <72034970+yrnmsk@users.noreply.github.com>
2023-07-22 15:33:17 +0800

    feat(page): improve shadow feel
    * feat: change shadow color
    * feat: switch to inner shadow initially
    * feat: on-hover shadow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
 
   return (
     <button
-      className='px-4 py-3 bg-gradient-to-br from-sky-500/75 from-60% to-indigo-500/50 shadow-md shadow-slate-950 transition-all ease-out duration-75 text-slate-50 border border-sky-900 rounded-lg flex flex-row gap-2 justify-center items-center md:px-6 md:py-4 md:rounded-xl md:text-lg hover:from-sky-500/50 hover:from-30% hover:to-indigo-500/30 hover:border-sky-400 hover:shadow-indigo-900 hover:translate-x-1 hover:-translate-y-1'
+      className='px-4 py-3 bg-gradient-to-br from-sky-500/75 from-60% to-indigo-500/50 shadow-inner shadow-indigo-300 transition-all ease-out duration-75 text-slate-50 border border-sky-900 rounded-lg flex flex-row gap-2 justify-center items-center md:px-6 md:py-4 md:rounded-xl md:text-lg hover:from-sky-500/50 hover:from-30% hover:to-indigo-500/30 hover:border-sky-400 hover:shadow-lg hover:shadow-indigo-950 hover:translate-x-1 hover:-translate-y-1'
       onClick={connectMetamask}
     >
       <span>Connect with MetaMask</span>
