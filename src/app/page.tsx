a982ad35d6616e91267a3242f1f0d070cfced371
Youka <72034970+yrnmsk@users.noreply.github.com>
2023-07-22 20:14:25 +0800

    feat(page): add simple card wrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,12 +51,18 @@ export default function Page() {
   }
 
   return (
-    <button
-      className='px-4 py-3 bg-gradient-to-br from-sky-500/75 from-60% to-indigo-500/50 shadow-inner shadow-indigo-300 transition-all ease-out duration-75 text-slate-50 border border-sky-900 rounded-lg flex flex-row gap-2 justify-center items-center md:px-6 md:py-4 md:rounded-xl md:text-lg hover:from-sky-500/50 hover:from-30% hover:to-indigo-500/30 hover:border-sky-400 hover:shadow-lg hover:shadow-indigo-950 hover:translate-x-1 hover:-translate-y-1'
-      onClick={connectMetamask}
-    >
-      <span>Connect with MetaMask</span>
-      <Image className='h-auto' src='/icon-metamask.svg' alt='metamask logo' width={32} height={32} />
-    </button>
+    <div className='flex flex-col gap-6 px-10 py-8 bg-slate-800 rounded-2xl'>
+      <div className='flex flex-col gap-3'>
+        <h3 className='text-slate-200 text-2xl font-bold'>Connect wallet</h3>
+        <hr className='border-slate-600'/>
+      </div>
+      <button
+        className='px-4 py-3 bg-gradient-to-br from-sky-500/75 from-60% to-indigo-500/50 shadow-inner shadow-indigo-300 transition-all ease-out duration-75 text-slate-50 border border-sky-900 rounded-lg flex flex-row gap-2 justify-center items-center md:px-6 md:py-4 md:rounded-xl md:text-lg hover:from-sky-500/50 hover:from-30% hover:to-indigo-500/30 hover:border-sky-400 hover:shadow-lg hover:shadow-indigo-950 hover:translate-x-2 hover:-translate-y-2'
+        onClick={connectMetamask}
+      >
+        <span>Connect with Metamask</span>
+        <Image className='h-auto' src='/icon-metamask.svg' alt='metamask logo' width={32} height={32} />
+      </button>
+    </div>
   );
 }
